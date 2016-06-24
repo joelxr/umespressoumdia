0f9901372adf17e4923fef8a6c52cd1497f8c2f4
Joel Rocha <joelxr@gmail.com>
2016-06-24 11:03:40 -0300

    script de deploy

--- deploy.bash
+++ deploy.bash
@@ -1,35 +1,22 @@
 #!/usr/bin/env bash
-set -e
+set -o errexit -o nounset
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
+rev=$(git rev-parse --short HEAD)
 
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+cd stage/_site
 
-rm -rf out/**/* || exit 0
+git init
+git config user.name "Joel Rocha"
+git config user.email "jeolxr@gmail.com"
 
-#grunt build
+git remote add upstream "https://$GH_TOKEN@github.com/joelxr/umespressoumdia.git"
+git fetch upstream
+git reset upstream/gh-pages
 
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+#echo "http://" > CNAME
 
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+touch .
 
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-openssl aes-256-cbc -K $encrypted_5bae8f0a9a70_key -iv $encrypted_5bae8f0a9a70_iv -in private_key.enc -out private_key -d
-chmod 600 private_key
-eval `ssh-agent -s`
-ssh-add private_key
-
-git push $SSH_REPO $TARGET_BRANCH
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
