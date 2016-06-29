c9dbe194da030bfbfd2d0d97c618b46c2f39adfd
Joel Rocha <jeolxr@gmail.com>
2016-06-29 19:57:37 +0000

    rebuild pages at b4e97c0

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -4,14 +4,12 @@ module.exports = function(grunt){
             files: {
                 'assets/js/jquery.js' : 'assets/vendor/jquery/dist/jquery.js',
                 'assets/js/materialize.js' : 'assets/vendor/Materialize/dist/js/materialize.js',
-                'assets/js/angular.js' : 'assets/vendor/angular/angular.js',
             }
         },
         dev: {
             files: {
                 '_site/assets/js/jquery.js' : 'assets/vendor/jquery/dist/jquery.js',
                 '_site/assets/js/materialize.js' : 'assets/vendor/Materialize/dist/js/materialize.js',
-                '_site/assets/js/angular.js' : 'assets/vendor/angular/angular.js',
             }
         }
     });
