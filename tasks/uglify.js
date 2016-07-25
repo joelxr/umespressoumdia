d5a1a68c549d1e7c9f7530b65a2572b011e3b486
Joel Rocha <joelxr@gmail.com>
2016-07-25 06:44:10 -0300

    Alterações do mapa dos cafés

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -5,6 +5,8 @@ module.exports = function(grunt){
                 'assets/js/jquery.js' : 'assets/vendor/jquery/dist/jquery.js',
                 'assets/js/materialize.js' : 'assets/vendor/Materialize/dist/js/materialize.js',
                 'assets/js/mapbox.js' : 'assets/vendor/mapbox.js/mapbox.js',
+                'assets/js/leaflet.js' : 'assets/vendor/leaflet/dist/leaflet-src.js',
+                'assets/js/leaflet.markercluster.js' : 'assets/vendor/leaflet.markercluster/dist/leaflet.markercluster-src.js',
             }
         },
         dev: {
@@ -12,6 +14,8 @@ module.exports = function(grunt){
                 '_site/assets/js/jquery.js' : 'assets/vendor/jquery/dist/jquery.js',
                 '_site/assets/js/materialize.js' : 'assets/vendor/Materialize/dist/js/materialize.js',
                 '_site/assets/js/mapbox.js' : 'assets/vendor/mapbox.js/mapbox.js',
+                '_site/assets/js/leaflet.js' : 'assets/vendor/leaflet/dist/leaflet.js',
+                '_site/assets/js/leaflet.markercluster.js' : 'assets/vendor/leaflet.markercluster/dist/leatlet.markercluster.js',
             }
         }
     });
